cd6b5d71900d21adcb563a3133b9ab3b4c6eb4cf
Marcelo d'Amorim <mdamori@ncsu.edu>
2025-04-18 08:55:17 -0400

    adding scripts directory

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,9 +1,6 @@
-THISDIR=`dirname "$(realpath "$0")"`
-export PYTHONPATH=$THISDIR:$PYTHONPATH
-
 # create virtual environment (i don't want to mess up abid's code)
 python -m venv venv
 source venv/bin/activate
 pip install -r $THISDIR/requirements.txt
 
-pytest
+(cd ..; pytest)
