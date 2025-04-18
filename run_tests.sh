6bdc2aecd0dd7264f7473f7629340ae6d2ac7c1c
abidnazirisami <abidnazirisami@gmail.com>
2025-04-18 02:16:47 -0400

    Added pytest to the requirements

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,8 @@ THISDIR=`dirname "$(realpath "$0")"`
 export PYTHONPATH=$THISDIR:$PYTHONPATH
 
 # create virtual environment (i don't want to mess up abid's code)
+python -m venv venv
 source venv/bin/activate
+pip install -r $THISDIR/requirements.txt
 
 pytest
