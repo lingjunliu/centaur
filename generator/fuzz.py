38b65045d3654c7f6760026740b56871a7c7376f
M M Abid Naziri <abidnazirisami@gmail.com>
2025-04-23 01:29:01 -0400

    Updated fuzzer

--- generator/fuzz.py
+++ generator/fuzz.py
@@ -2,12 +2,12 @@ import sys
 from .harness import run_api_with_duration
 
 def main():
-    if len(sys.args) < 3:
+    if len(sys.argv) < 3:
         print("Usage: python fuzz.py <api> <duration>")
         return
     
-    api = sys.args[1]
-    duration = int(sys.args[2])
+    api = sys.argv[1]
+    duration = int(sys.argv[2])
     
     run_api_with_duration(api, duration)
 
--- scripts/fuzz_with_slurm.sh
+++ scripts/fuzz_with_slurm.sh
@@ -8,9 +8,9 @@ duration=${1:-300}  # seconds
 print_progress(){
     job_name=$1
     elapsed=$2
-    remaining=$(squeue --user=$USER | grep -c $job_name | wc -l)
-    pending=$(squeue --user=$USER --state=PENDING | grep -c $job_name | wc -l)
-    running=$(squeue --user=$USER --state=RUNNING | grep -c $job_name | wc -l)
+    remaining=$(squeue --user=$USER | grep -vE "JOBID" | grep "${job_name}" | wc -l)
+    pending=$(squeue --user=$USER --state=PENDING | grep -vE "JOBID" | grep "${job_name}" | wc -l)
+    running=$(squeue --user=$USER --state=RUNNING | grep -vE "JOBID" | grep "${job_name}" | wc -l)
     echo "${remaining} jobs remaining in the slurm queue | ${running} running, ${pending} waiting to run | ${elapsed} seconds elapsed"
 }
 
@@ -18,6 +18,7 @@ print_progress(){
 
 PROJECT_DIR=`dirname "$(realpath "$0")"`/..
 export PYTHONPATH=$PROJECT_DIR:$PYTHONPATH
+export PYTHONWARNINGS="ignore"
 
 # Creating virtual environment
 python -m venv venv
@@ -31,26 +32,26 @@ i=0
 elapsed=0
 max_parallel=60
 mkdir -p logs
-job_name=fuzz_api-
+job_name=fuzz_api
 
 for api in $(cat apis.txt); do
     sbatch -c 1 \
-        --job-name="${job_name}${i}" \
+        --job-name=${job_name} \
         --output="logs/${api}.out" \
-        --wrap="srun --cpu-bind=cores PYTHONWARNINGS='ignore' python -m generator.fuzz ${api} ${duration}"
+        --wrap="srun --cpu-bind=cores python -m generator.fuzz ${api} ${duration}"
     ((i++))
 
     # limit number of running jobs
-    while (( $(squeue -u $USER | grep -c ${job_name}) >= max_parallel )); do
+    while (( $(squeue --user=$USER | grep -vE "JOBID" | grep "${job_name}" | wc -l) >= max_parallel )); do
+        print_progress ${job_name} ${elapsed}
         sleep 10
         (( elapsed = elapsed + 10 ))
-        print_progress ${job_name} ${elapsed}
     done
 done
 
 # wait for everything to finish
-while (( $(squeue -u $USER | grep -c 'fuzz_api-') >= 0 )); do
+while (( $(squeue --user=$USER | grep -vE "JOBID" | grep "${job_name}" | wc -l) > 0 )); do
+    print_progress ${job_name} ${elapsed}
     sleep 10
     (( elapsed = elapsed + 10 ))
-    print_progress ${job_name} ${elapsed}
 done
