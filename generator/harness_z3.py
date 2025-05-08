914e95ad728b3ddaacdbceaaf84520333f329ecf
M M Abid Naziri <abidnazirisami@gmail.com>
2025-05-07 23:40:17 -0400

    Fixed message

--- generator/harness_z3.py
+++ generator/harness_z3.py
@@ -198,7 +198,7 @@ def run_api_with_duration(api, model_gen_duration, fuzz_duration, max_model, n_m
     if not os.path.exists(models_file) or model_regen:
         models = gen_models(definition, driver, z3_args, model_gen_duration, max_model)
         if len(models) == 0:
-            print(f"No models generated for {api} within the {model_gen_duration} time budget")
+            print(f"No models generated for {api} within the {model_gen_duration} second time budget")
             return
         # save models to a file
         save_to_new_pkl(models_file, models)
@@ -264,13 +264,13 @@ if __name__ == "__main__":
     max_model = 100
     print_details = sys.argv[1].lower() == 'true' if len(sys.argv) > 1 else False
     
-    run_api_with_duration("scatter", model_gen_duration, fuzz_duration, max_model, print_details=print_details)
+    run_api_with_duration("kthvalue", model_gen_duration, fuzz_duration, max_model, print_details=print_details)
      
-    # Run atan2 for 30 seconds
-    run_api_with_duration("atan2", model_gen_duration, fuzz_duration, max_model, print_details=print_details)
+    # # Run atan2 for 30 seconds
+    # run_api_with_duration("atan2", model_gen_duration, fuzz_duration, max_model, print_details=print_details)
     
-    # Run argmin for 30 seconds
-    run_api_with_duration("argmin", model_gen_duration, fuzz_duration, max_model, print_details=print_details)
+    # # Run argmin for 30 seconds
+    # run_api_with_duration("argmin", model_gen_duration, fuzz_duration, max_model, print_details=print_details)
     
-    # Run conv_transpose2d for 30 seconds
-    run_api_with_duration("conv_transpose2d", model_gen_duration, fuzz_duration, max_model, print_details=print_details)
+    # # Run conv_transpose2d for 30 seconds
+    # run_api_with_duration("conv_transpose2d", model_gen_duration, fuzz_duration, max_model, print_details=print_details)
