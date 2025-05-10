e89c3556170707ab25f4d1efd3fef227eeff8ed8
abidnazirisami <abidnazirisami@gmail.com>
2025-05-09 23:59:45 -0400

    Updated input analyzer

--- eval/analyze_inputs.py
+++ eval/analyze_inputs.py
@@ -33,18 +33,16 @@ def main():
             print(f"Index {ind} out of range for generated inputs.")
             return
         best_distance, abs_input, seed = generated_inputs[ind]
-        oracle_result = oracle_results[ind]
         
-        if oracle_result[0] == "nominal":
-            print(f"Input {ind} is nominal.")
-        elif oracle_result[0] == "invalid":
-            print(f"Input {ind} is invalid: {oracle_result[1]}")
-        elif oracle_result[0] == "cpu_crash":
-            print(f"CPU crash in input {ind}: {oracle_result[1]}")
-        elif oracle_result[0] == "gpu_crash":
-            print(f"GPU crash in input {ind}: {oracle_result[1]}")
-        elif oracle_result[0] == "inconsistent":
-            print(f"Inconsistent output for input {ind}: {oracle_result[1]}")
+        if ind < len(oracle_results):
+            oracle_result = oracle_results[ind]
+            
+            if oracle_result[0] == "nominal":
+                print(f"Input {ind} is nominal.")
+            elif oracle_result[0] == "invalid":
+                print(f"Input {ind} is invalid: {oracle_result[1]}")
+            else:
+                print(f"{ind}: {'\n'.join([str(x) for x in oracle_result])}")
             
         print("\nRe running the oracle...")
         driver = get_driver(api)
@@ -54,19 +52,17 @@ def main():
         diff_oracle_result = oracle_diff(driver, signature, input_dict, atol=A_TOL)
         
         print(f"\nOracle result: {diff_oracle_result}")
-        print(f"\nAbstract input: {abstract_print(abs_input, signature)}")
+        print(f"\nAbstract input (seed {seed}): {abstract_print(abs_input, signature)}")
     else:
+        print(f"Total inputs: {len(generated_inputs)}")
+        print(f"Total oracles: {len(oracle_results)}")
         for i, oracle_result in enumerate(oracle_results):
             if oracle_result[0] == "nominal":
                 continue
             elif oracle_result[0] == "invalid":
                 continue
-            elif oracle_result[0] == "cpu_crash":
-                print(f"CPU crash in input {i}: {oracle_result[1]}")
-            elif oracle_result[0] == "gpu_crash":
-                print(f"GPU crash in input {i}: {oracle_result[1]}")
-            elif oracle_result[0] == "inconsistent":
-                print(f"Inconsistent output for input {i}: {oracle_result[1]}")
+            else:
+                print(f"{i}: {'\n'.join([str(x) for x in oracle_result])}\n")
 
 if __name__ == "__main__":
     main()
