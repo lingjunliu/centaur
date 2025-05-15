3cb4f7fa20ee01108a389690f31b965368df3460
M M Abid Naziri <abidnazirisami@gmail.com>
2025-05-14 21:27:45 -0400

    Modified inference script

--- scripts/infer_invariants_with_slurm.sh
+++ scripts/infer_invariants_with_slurm.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 duration=${1:-300}  # seconds
+regen=${2:-0}       # 1 means force invariant regenration
 
 job_name=infer
 slurm_sh=`dirname "$(realpath "$0")"`/slurm_base.sh # base script for slurm
 
-bash $slurm_sh "python -m learner.invariant_inference" ${job_name} ${duration}
+bash $slurm_sh "python -m learner.invariant_inference" ${job_name} ${duration} ${regen}
 
 # Aggregating and saving results
 PROJECT_DIR=`dirname "$(realpath "$0")"`/..
