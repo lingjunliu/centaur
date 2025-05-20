095d19b2f9923a5a5c44f3cccbd0aa4c7587340a
abidnazirisami <abidnazirisami@gmail.com>
2025-05-20 06:15:34 -0400

    Added a potential tf bug

---
+++ bug_reports/tf/conv_transpose2d.py
@@ -0,0 +1,72 @@
+"""
+[cpu_crash]
+SIGILL
+
+Abstract input:
+input: 
+	shape: (1, 4, 4, 5)
+	dtype: <class 'numpy.float32'>
+	range: (-3.9808033, -3.0141976)
+weight: 
+	shape: (4, 4, 4, 4)
+	dtype: <class 'numpy.float32'>
+	range: (2.0, 2.0)
+stride: 
+	value: 2
+	dtype: <class 'numpy.int8'>
+padding: 
+	value: 3
+	dtype: <class 'numpy.int8'>
+# from utils.api_utils import get_driver
+
+# api = "conv_transpose2d"
+# driver = get_driver(api, lib="tf")
+
+
+
+# input = {
+#     "input": rng.uniform(-3.9808033, -3.0141976, (1, 4, 4, 5)).astype(np.float32),
+#     "weight": rng.uniform(2.0, 2.0, (4, 4, 4, 4)).astype(np.float32),
+#     "stride": np.int8(2),
+#     "padding": np.int8(3)
+# }
+
+# output = driver(input_dict, cpu=True)
+    # print(output)
+"""
+
+import tensorflow as tf
+import numpy as np
+rng = np.random.default_rng(40)
+
+tf.config.experimental.enable_op_determinism()
+
+with tf.device("/CPU:0"):
+    # Unpack input dictionary
+    input_tensor = tf.constant(rng.uniform(-3.9808033, -3.0141976, (1, 4, 4, 5)), dtype=tf.float32)
+    weight_tensor = tf.constant(rng.uniform(2.0, 2.0, (4, 4, 4, 4)), dtype=tf.float32)
+    stride = 2
+    padding = 3
+    output_padding = 0
+    groups = 1
+    dilation = 1
+    
+    input_tensor = tf.transpose(input_tensor, [0, 2, 3, 1])
+    weight_tensor = tf.transpose(weight_tensor, [2, 3, 1, 0])  # Adjust weight shape for TensorFlow format
+
+    batch_size = tf.shape(input_tensor)[0]
+    input_height = input_tensor.shape[1]
+    input_width = input_tensor.shape[2]
+    filter_height = weight_tensor.shape[0]
+    filter_width = weight_tensor.shape[1]
+    
+    out_channels = weight_tensor.shape[3]
+    
+    h_out = (input_height - 1) * stride - 2 * padding + dilation * (filter_height - 1) + 1 + output_padding
+    w_out = (input_width - 1) * stride - 2 * padding + dilation * (filter_width - 1) + 1 + output_padding
+    output_shape = [batch_size, h_out, w_out, out_channels]
+
+    output = tf.nn.conv2d_transpose(
+        input_tensor, weight_tensor, output_shape=output_shape, strides=[1, stride, stride, 1],
+        padding='VALID' if padding == 0 else 'SAME', dilations=[1, dilation, dilation, 1]
+    )
