86bbe6af4b69a37d09811ef56d5e493da250a1f8
abidnazirisami <abidnazirisami@gmail.com>
2025-05-20 03:29:12 -0400

    Added tensorflow version to scatter

--- drivers/scatter.py
+++ drivers/scatter.py
@@ -23,4 +23,44 @@ def torch_version(input_dict, cpu=True):
     
     return {"result": result.numpy()}
 
-# TODO: Implement the TensorFlow version of the scatter driver function
+def tensorflow_version(input_dict):
+    import tensorflow as tf
+    input_tensor = tf.constant(input_dict["input"])
+    dim = input_dict["dim"]
+    index = tf.constant(input_dict["index"])
+    src = tf.constant(input_dict["src"])
+
+    input_shape = tf.shape(input_tensor)
+    batch_dims = tf.meshgrid(*[tf.range(s) for s in input_shape], indexing='ij')
+
+    if dim == 0:
+        scatter_indices = tf.stack([index, batch_dims[1]], axis=-1)
+    elif dim == 1:
+        scatter_indices = tf.stack([batch_dims[0], index], axis=-1)
+    else:
+        raise ValueError("Only dim 0 or 1 supported for 2D tensors.")
+
+    scatter_indices = tf.cast(scatter_indices, tf.int64)
+    result = tf.tensor_scatter_nd_update(input_tensor, scatter_indices, src)
+
+    return {"result": result.numpy()}
+
+def main():
+    import numpy as np
+
+    # Sample 2D input example
+    input_dict = {
+        "input": [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]],
+        "dim": 1,
+        "index": [[0, 1, 2], [2, 0, 1]],
+        "src": [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]
+    }
+
+    torch_output = torch_version(input_dict)
+    tf_output = tensorflow_version(input_dict)
+
+    np.testing.assert_allclose(torch_output["result"], tf_output["result"], atol=0.01)
+    print("Outputs match within tolerance.")
+
+if __name__ == "__main__":
+    main()
