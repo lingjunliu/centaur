3fbed1cfb20a7d65591f1ab2c75cd68e33c233f0
abidnazirisami <abidnazirisami@gmail.com>
2025-05-22 04:32:43 -0400

    Updated API stats code to add library support

--- learner/stats_inv.py
+++ learner/stats_inv.py
@@ -1,10 +1,18 @@
-import os
+import os, sys
 from utils.misc import get_dir_in_root, get_tmp_dir, read_file_in_root
 
 def main():
+    lib = sys.argv[1] if len(sys.argv) > 1 else "torch"
+
+    # alias
+    if lib == "pytorch":
+        lib = "torch"
+    elif lib == "tensorflow":
+        lib = "tf"
+
     rule_to_api = {}
     api_to_rule = {}
-    inv_dir = get_dir_in_root("invariants")
+    inv_dir = get_dir_in_root(f"invariants_{lib}")
     for file in os.listdir(inv_dir):
         file_path = os.path.join(inv_dir, file)
         if os.path.isfile(file_path):
@@ -24,8 +32,8 @@ def main():
                     api_to_rule[api].add(rule)
     
     tmp = get_tmp_dir()
-    rule_to_api_csv = os.path.join(tmp, "rule_to_api.csv")
-    api_to_rule_csv = os.path.join(tmp, "api_to_rule.csv")
+    rule_to_api_csv = os.path.join(tmp, f"rule_to_api_{lib}.csv")
+    api_to_rule_csv = os.path.join(tmp, f"api_to_rule_{lib}.csv")
     
     with open(rule_to_api_csv, "w") as f:
         for rule, apis in rule_to_api.items():
@@ -36,9 +44,12 @@ def main():
             f.write(f"{api},{len(rules)}\n")
             
     supported_apis = read_file_in_root("apis.txt")
-    print("The following apis do not have invariants yet:")
+    to_print = True
     for api in supported_apis:
         if api.strip() not in api_to_rule:
+            if to_print:
+                print("The following apis do not have invariants yet:")
+                to_print = False
             print(api.strip())
 
 if __name__ == "__main__":
