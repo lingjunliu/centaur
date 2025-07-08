2aeac4f8b27796951be281978c16b1b258c0c1d7
M M Abid Naziri <abidnazirisami@gmail.com>
2025-07-08 01:22:27 -0400

    Fixed issue with merging

--- generator/z3.py
+++ generator/z3.py
@@ -316,7 +316,6 @@ def reduce_ruleset(definition, api, z3_args, max_trial=30, print_details=False, 
     base_validity_ratio = 0.0
 
     for rule in [None] + list(ruleset):
-    for rule in [None] + list(ruleset):
         trial = 0
         valid = 0
         valid = 0
