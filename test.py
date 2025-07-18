5c5e23191d804d1a4af85025ffc0a58f4d9a8768
M M Abid Naziri <abidnazirisami@gmail.com>
2025-07-18 06:40:23 -0400

    Updated readme

--- test.py
+++
@@ -1,7 +0,0 @@
-from llm.valid_inputs_tf import generated_inputs
-import random
-
-selected_apis = random.sample(list(generated_inputs.keys()), 10)
-
-print("Selected APIs:")
-print('\n'.join(selected_apis))
