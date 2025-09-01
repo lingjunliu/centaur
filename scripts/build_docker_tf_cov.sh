d7055a317f38274219990572bbfee6008fc21dbb
M M Abid Naziri <abidnazirisami@gmail.com>
2025-09-01 00:30:28 -0400

    Parameterized docker build variables

--- scripts/build_docker_tf_cov.sh
+++ scripts/build_docker_tf_cov.sh
@@ -1,6 +1,6 @@
 # Collect coverage using Docker (Put resource limits here)
-export max_parallel=100         # Maximum number of parallel jobs (set this based on the number of slurm jobs you want to spawn to run at the same time)
-export max_memory_docker=400G   # Maximum memory for Docker container for TensorFlow Coverage (set this based on the memory you want to allocate for Docker)
+export max_parallel=${1:-100}         # Maximum number of parallel jobs (set this based on the number of slurm jobs you want to spawn to run at the same time)
+export max_memory_docker=${2:-400G}   # Maximum memory for Docker container for TensorFlow Coverage (set this based on the memory you want to allocate for Docker)
 
 docker build -t tf_216_instr_im . -f instrumented_tf/Dockerfile
 docker run \
