73b51a40e8c1b86f6be75f735a09285f7c752d62
M M Abid Naziri <abidnazirisami@gmail.com>
2025-11-10 17:02:12 -0500

    Added a general script for llm experiments

---
+++ llm_exp.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+run_llm() {
+    lib=$1
+    llm=$2
+    regen=1
+    # APIS
+    cp llm/$llm-$lib/$lib_finalized_apis.txt $lib_apis.txt
+    cp llm/$llm-$lib/$lib_variations.txt .
+    # Set num_p to the number of lines in the variations file
+    num_p=$(wc -l < $lib_variations.txt)
+    echo "--------------------------------"
+    echo "$llm: Number of parallel jobs for Slurm: $num_p"
+    echo "--------------------------------"
+
+    # Inputs and signatures
+    cp llm/$llm-$lib/valid_inputs_$lib.py llm/
+    cp llm/$llm-$lib/signatures.json .
+
+    # Rules
+    rm -r rules-$lib
+    cp -r rulegen/$llm-$lib/rules-$lib .
+
+    # Clear corpus
+    rm -r corpus_$lib/*
+
+    echo "--------------------------------"
+    echo "Running pipeline for $llm on $lib..."
+    echo "--------------------------------"
+    bash pipeline.sh $lib 1 1 ${lib}_$llm $regen $num_p
+}
+
+# Torch, Claude
+lib=torch
+llm=claude
+run_llm $lib $llm
+
