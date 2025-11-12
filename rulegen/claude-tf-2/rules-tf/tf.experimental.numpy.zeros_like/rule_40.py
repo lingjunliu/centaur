be21fa34aba2404480350d8271008291b809882c
canarypwn <canarypwn@aosc.io>
2025-11-12 15:14:58 -0500

    claude-tf-2

---
+++ rulegen/claude-tf-2/rules-tf/tf.experimental.numpy.zeros_like/rule_40.py
@@ -0,0 +1,44 @@
+import numpy as np
+import torch 
+import tensorflow as tf
+
+from utils.defaults import MAX_N_DIM, MAX_SZ_DIM, MAX_SZ_NUM, list_of_available_dtypes, list_of_string_values_tf, np_dtype
+from z3 import *
+
+# high precision dtype for large tensors (Rule 40)
+
+rule_40 = lambda s, v, n=False: (
+    s.add(Not(If(And([Implies(i < (v["arg1_ndim"] - 1 + 1), Select(v["arg1_shape"], i) > 1000) for i in range(6)]), Or(v["arg2_value"] >= 4, v["arg2_value"] == 8), True)) if n else
+          If(And([Implies(i < (v["arg1_ndim"] - 1 + 1), Select(v["arg1_shape"], i) > 1000) for i in range(6)]), Or(v["arg2_value"] >= 4, v["arg2_value"] == 8), True))
+)
+
+def rule_40_func(arg1, arg2, solver=None, neg=False):
+    arg1 = next(iter(arg1.values()))
+    arg2 = next(iter(arg2.values()))
+
+    # Invariant learning phase
+    if not solver:
+        if not isinstance(arg1, np.ndarray):
+            return False
+        if not (isinstance(arg2, torch.dtype) or isinstance(arg2, tf.dtypes.DType)):
+            return False
+
+        # Variable declarations
+        solver = Solver()
+        arg1_ndim = Int('arg1_ndim')
+        arg1_shape = Array('arg1_shape', IntSort(), IntSort())
+        arg2_value = Int('arg2_value')
+
+        # Value assignments
+        solver.add(arg1_ndim == arg1.ndim)
+        for i in range(arg1.ndim):
+            arg1_shape = Store(arg1_shape, i, arg1.shape[i])
+        solver.add(arg2_value == list_of_available_dtypes.index(np_dtype(arg2)))
+
+        # Constraints for rule 40
+        rule_40(solver, {'arg1_ndim': arg1_ndim, 'arg1_shape': arg1_shape, 'arg2_value': arg2_value})
+        return solver.check() == sat
+
+    # Fuzz input generation phase
+    else:
+        rule_40(solver, {'arg1_ndim': arg1['ndim'], 'arg1_shape': arg1['shape'], 'arg2_value': arg2['value']}, neg)
