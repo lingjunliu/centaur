1e2db3fc0539878352cb52d9724fc45cb192427c
manideepika21 <mmyaka21@gmail.com>
2026-04-07 19:20:17 -0400

    Added signatures, inputs, rules and invariants for the new apis

---
+++ rulegen/gemini/rules-tf-new-apis/tf.math.approx_max_k/rule_49.py
@@ -0,0 +1,51 @@
+import numpy as np
+import torch 
+import tensorflow as tf
+
+from utils.defaults import MAX_N_DIM, MAX_SZ_DIM, MAX_SZ_NUM, list_of_available_dtypes, list_of_string_values_tf, np_dtype
+from z3 import *
+
+# operand must be a floating number type, k must be greater than 0, recall_target must be between 0 and 1, exclusive, reduction_input_size_override must be a non-negative integer (Rule 49)
+
+rule_49 = lambda s, v, n=False: (
+    s.add(Not(And(And(And(And(And((Or(Or(v["arg1_dtype"] == 6, v["arg1_dtype"] == 7), v["arg1_dtype"] == 8)), v["arg2_value"] > 0), 0 < v["arg3_value"]), v["arg3_value"] < 1), v["arg4_value"] >= 0), If(v["arg4_value"] > 0, v["arg4_value"] > v["arg2_value"], True))) if n else
+          And(And(And(And(And((Or(Or(v["arg1_dtype"] == 6, v["arg1_dtype"] == 7), v["arg1_dtype"] == 8)), v["arg2_value"] > 0), 0 < v["arg3_value"]), v["arg3_value"] < 1), v["arg4_value"] >= 0), If(v["arg4_value"] > 0, v["arg4_value"] > v["arg2_value"], True)))
+)
+
+def rule_49_func(arg1, arg2, arg3, arg4, solver=None, neg=False):
+    arg1 = next(iter(arg1.values()))
+    arg2 = next(iter(arg2.values()))
+    arg3 = next(iter(arg3.values()))
+    arg4 = next(iter(arg4.values()))
+
+    # Invariant learning phase
+    if not solver:
+        if not isinstance(arg1, np.ndarray):
+            return False
+        if not (isinstance(arg2, (int, np.integer)) and not isinstance(arg2, bool)):
+            return False
+        if not isinstance(arg3, (float, np.floating)):
+            return False
+        if not (isinstance(arg4, (int, np.integer)) and not isinstance(arg4, bool)):
+            return False
+
+        # Variable declarations
+        solver = Solver()
+        arg1_dtype = Int('arg1_dtype')
+        arg2_value = Int('arg2_value')
+        arg3_value = Real('arg3_value')
+        arg4_value = Int('arg4_value')
+
+        # Value assignments
+        solver.add(arg1_dtype == list_of_available_dtypes.index(arg1.dtype))
+        solver.add(arg2_value == int(arg2))
+        solver.add(arg3_value == arg3)
+        solver.add(arg4_value == int(arg4))
+
+        # Constraints for rule 49
+        rule_49(solver, {'arg1_dtype': arg1_dtype, 'arg2_value': arg2_value, 'arg3_value': arg3_value, 'arg4_value': arg4_value})
+        return solver.check() == sat
+
+    # Fuzz input generation phase
+    else:
+        rule_49(solver, {'arg1_dtype': arg1['dtype'], 'arg2_value': arg2['value'], 'arg3_value': arg3['value'], 'arg4_value': arg4['value']}, neg)
